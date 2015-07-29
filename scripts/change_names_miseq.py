d6bbdb82fba6bdaee8530b85d625512625f2320f
JT McCrone <mccrone@umich.edu>
2015-07-29 09:23:23 -0400

    Update change_names_miseq.py
    Added ability to change the name of .gz files with a boolean switch

--- scripts/change_names_miseq.py
+++ scripts/change_names_miseq.py
@@ -7,11 +7,13 @@ parser = argparse.ArgumentParser(description='This program takes Miseq fastq fil
 parser.add_argument('-s',action='store',dest='s',help='The sorce directory containing the original fastq files')
 parser.add_argument('-f',action='store',dest='f',help='The final directory that will hold the renamed fastq files')
 parser.add_argument('-run',action='store_true',dest='test',default=False,help='Boolean switch to run program, without this the program runs in test mode: the log is made but no files are renamed')
+parser.add_argument('-gz',action='store_true',dest='zip',default=False,help='Boolean switch. Activate when working with .gz files')
 
 args=parser.parse_args()
 s=args.s
 f=args.f
 test=args.test
+zip=args.zip
 # input argument is fastq directory
 
 if not os.path.exists(f):
@@ -28,7 +30,10 @@ for filename in os.listdir(s):
     read_number=lane_junk.split("_R")
     fastq_number=read_number[1][4]
     read_number=read_number[1][0]
-    perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq"
+    if zip==True:
+        perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq.gz"
+    else:
+        perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq.gz"
     # Write file to new name
 
     print("COPYING "+ s+filename + " to "+f+perfect_name)

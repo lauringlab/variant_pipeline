b4b5f57cd1c24b5c5c82f6e47e76bd93ddc78251
jtmccr1 <mccrone@umich.edu>
2015-07-29 15:12:42 -0400

    work on renamining file

--- scripts/change_names_miseq.py
+++ scripts/change_names_miseq.py
@@ -2,8 +2,10 @@
 import os
 import argparse
 import shutil
+import gzip
+import glob
 
-parser = argparse.ArgumentParser(description='This program takes Miseq fastq files and renames them as sample.read_direction.#.fastq and keeps a log of the change')
+parser = argparse.ArgumentParser(description='This program takes Miseq fastq files and renames them as sample.read_direction.#.fastq and keeps a log of the change. If the initial files are gzipped it will unzip the copies.')
 parser.add_argument('-s',action='store',dest='s',help='The sorce directory containing the original fastq files')
 parser.add_argument('-f',action='store',dest='f',help='The final directory that will hold the renamed fastq files')
 parser.add_argument('-run',action='store_true',dest='test',default=False,help='Boolean switch to run program, without this the program runs in test mode: the log is made but no files are renamed')
@@ -13,7 +15,7 @@ args=parser.parse_args()
 s=args.s
 f=args.f
 test=args.test
-zip=args.zip
+#zip=args.zip
 # input argument is fastq directory
 
 if not os.path.exists(f):
@@ -22,7 +24,7 @@ if not os.path.exists(f):
 outfile = open(f+'renaming_log.txt','w')
 #os.chdir(sys.argv[1])
 if test==False:
-    print "running in test mode add option -r to run"
+    print "running in test mode add option -run to run"
 for filename in os.listdir(s):
  #   print filename
     name=filename.split("_L")
@@ -31,16 +33,26 @@ for filename in os.listdir(s):
     read_number=lane_junk.split("_R")
     fastq_number=read_number[1][4]
     read_number=read_number[1][0]
-    if zip==True:
-        perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq.gz"
-    else:
-        perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq"
+    ending=os.path.splitext(filename)[1]
+    if ending=='.gz':
+        ending='.fastq.gz'
+    perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+ending
+
     # Write file to new name
 
     print("COPYING "+ s+filename + " to "+f+perfect_name)
     outfile.write(s+filename + "\t COPIED to \t" + f+perfect_name + "\n")
     if test==True:
         shutil.copy(s+filename,f+perfect_name)
+    for file in glob.glob(f + "/*.gz"):
+
+        inF = gzip.GzipFile(file, 'rb')
+        s = inF.read()
+        inF.close()
+
+        outF = file(os.path.splitext(file)[0], 'wb')
+        outF.write(s)
+        outF.close()
 
 
 outfile.close()

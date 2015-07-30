19421cc15c038400fe1e4ffeeaf238b201ac26a5
jtmccr1 <mccrone@umich.edu>
2015-07-30 09:39:31 -0400

    robust miseq changename to handel zipped files

--- scripts/change_names_miseq.py
+++ scripts/change_names_miseq.py
@@ -5,11 +5,11 @@ import shutil
 import gzip
 import glob
 
-parser = argparse.ArgumentParser(description='This program takes Miseq fastq files and renames them as sample.read_direction.#.fastq and keeps a log of the change. If the initial files are gzipped it will unzip the copies.')
+parser = argparse.ArgumentParser(description='This program takes Miseq fastq files and renames them as sample.read_direction.#.fastq and keeps a log of the change. If the initial files are gzipped it will unzip the copies.\n It will unzip all gzipped files in the final (-f) directory.')
 parser.add_argument('-s',action='store',dest='s',help='The sorce directory containing the original fastq files')
 parser.add_argument('-f',action='store',dest='f',help='The final directory that will hold the renamed fastq files')
 parser.add_argument('-run',action='store_true',dest='test',default=False,help='Boolean switch to run program, without this the program runs in test mode: the log is made but no files are renamed')
-parser.add_argument('-gz',action='store_true',dest='zip',default=False,help='Boolean switch. Activate when working with .gz files')
+#parser.add_argument('-gz',action='store_true',dest='zip',default=False,help='Boolean switch. Activate when working with .gz files')
 
 args=parser.parse_args()
 s=args.s
@@ -21,12 +21,22 @@ test=args.test
 if not os.path.exists(f):
     os.makedirs(f)
 
+# add slash to final location if it was forgotten
+if f[-1] != '/':
+    f=f+'/'
+#print f
+
+if s[-1] != '/':
+    s=s+'/'
+#print s
+
 outfile = open(f+'renaming_log.txt','w')
 #os.chdir(sys.argv[1])
 if test==False:
     print "running in test mode add option -run to run"
-for filename in os.listdir(s):
- #   print filename
+# copy fastq files
+for filename in glob.glob(s + "*.fastq"):
+    filename= os.path.basename(filename)
     name=filename.split("_L")
     good_name = name[0].split("_")[0]
     lane_junk = name[1]
@@ -34,9 +44,8 @@ for filename in os.listdir(s):
     fastq_number=read_number[1][4]
     read_number=read_number[1][0]
     ending=os.path.splitext(filename)[1]
-    if ending=='.gz':
-        ending='.fastq.gz'
-    perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+ending
+
+    perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq"
 
     # Write file to new name
 
@@ -45,6 +54,25 @@ for filename in os.listdir(s):
     if test==True:
         shutil.copy(s+filename,f+perfect_name)
 #    print(f + "*.gz")
+
+for zipfilename in glob.glob(s + "*.fastq.gz"):
+    zipfilename= os.path.basename(zipfilename)
+    name=zipfilename.split("_L")
+    good_name = name[0].split("_")[0]
+    lane_junk = name[1]
+    read_number=lane_junk.split("_R")
+    fastq_number=read_number[1][4]
+    read_number=read_number[1][0]
+    ending=os.path.splitext(filename)[1]
+
+    perfect_name= good_name.replace("-","_")+"."+read_number+"."+fastq_number+".fastq.gz"
+
+    # Write file to new name
+
+    print("COPYING "+ s+zipfilename + " to "+f+perfect_name)
+    outfile.write(s+zipfilename + "\t COPIED to \t" + f+perfect_name + "\n")
+    if test==True:
+        shutil.copy(s+zipfilename,f+perfect_name)
 
 for zipfile in glob.glob(f + "*.gz"):
     print "unzipping:" + zipfile

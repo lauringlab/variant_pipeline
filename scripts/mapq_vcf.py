b9a62ffa8e71e544719577675847564e7735c2a6
jtmccr1 <mccrone@umich.edu>
2015-09-28 11:31:20 -0400

    mapq_vcf only makes a vcf and updates info

--- scripts/mapq_vcf.py
+++ scripts/mapq_vcf.py
@@ -5,6 +5,7 @@ import numpy as np
 import pysam
 from collections import Counter
 import vcf
+from vcf.parser import _Info as VcfInfo
 
 
 input=sys.argv[1].split(".")[0]
@@ -27,6 +28,15 @@ bam=bam.split("/")[-1]
 #Put header on true false and exp csv files
 
 in_var= vcf.Reader(open(sys.argv[1], 'r'))
+## update infos ##
+in_var.infos['MapQ']=VcfInfo(id='MapQ',num=1,type='Float',desc="The average MapQ of the reads containing the called variant",source=None, version=None)
+in_var.infos['Read_pos']=VcfInfo(id='Read_pos',num=1,type='Float',desc="The average read cycle that called the given variant",source=None, version=None)
+in_var.infos['Phred']=VcfInfo(id='Phred',num=1,type='Float',desc="The average Phred score of the called variant",source=None, version=None)
+
+
+
+
+
 variants=list(in_var)
 with pysam.AlignmentFile(sys.argv[2], "rb") as bamfile:
 
@@ -41,13 +51,14 @@ with pysam.AlignmentFile(sys.argv[2], "rb") as bamfile:
 		py_pos=pos-1
 		ref=record.REF
 		var=record.ALT[0]
+		record.ID=input
 		for pileupcolumn in bamfile.pileup(chr,py_pos,py_pos+1,truncate=True,stepper="all",max_depth=1E6):
 			if pileupcolumn.pos==py_pos:
 				for pileupread in pileupcolumn.pileups:
 					if  not pileupread.is_del and not pileupread.is_refskip:
 						called_base=pileupread.alignment.query_sequence[pileupread.query_position]
 						called_phred=pileupread.alignment.query_qualities[pileupread.query_position]
-						if called_phred>25 and called_base==var: # change this if you change the phred cut off in deepSNV
+						if called_phred>0 and called_base==var: # change this if you change the phred cut off in deepSNV
 							mapq.append(pileupread.alignment.mapping_quality)
 							phred.append(called_phred)
 							Read_pos.append(pileupread.query_position)
@@ -58,23 +69,15 @@ with pysam.AlignmentFile(sys.argv[2], "rb") as bamfile:
 			print( "OOPS didn't find the variant looks like you didn't fix the bug")
 			sys.exit(1)
 
-		record.INFO.update({'MAPQ':mean_map, 'Read_pos':mean_Read_pos, 'phred':mean_phred})
+		record.add_info('MAPQ',mean_map)
+		record.add_info('Read_pos',mean_Read_pos)
+		record.add_info('Phred',mean_phred)
 #         print record.INFO
 #         print 'done with \n'
 #         print record
 print "done updating"
 iter(variants)
 vcf_writer = vcf.Writer(open(sys.argv[3], 'w'), in_var)
-csv=open(sys.argv[4],'w')
-header="CHROM,POS,REF,ALT,QUAL,ID"
-for entry in record.INFO.keys():
-    header=header+","+entry
-csv.write(header+"\n")
 for record in variants:
-    line=record.CHROM+","+str(record.POS)+","+record.REF+","+str(record.ALT[0])+","+str(record.QUAL)+","+input
-    for entry in record.INFO:
-        line=line+","+str(record.INFO[entry]).replace(",",":")
-    csv.write(line+"\n")
-    vcf_writer.write_record(record)
-csv.close()
+	vcf_writer.write_record(record)
 vcf_writer.close()

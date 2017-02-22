e687525b2c25cd15a4c2c4d93990efea2716cdc5
jtmccr1 <mccrone@umich.edu>
2017-02-22 11:56:32 -0500

    prints out reference base on foward read - hopefully correct

--- scripts/reciprocal_variants.py
+++ scripts/reciprocal_variants.py
@@ -16,12 +16,38 @@ class test_args: # for debugging and testing
    
 #args=parser.parse_args()    
 args=test_args()
-variants=pd.read_csv(args.csv[0],index_col=0)
+variants=pd.read_csv(args.csv[0],index_col=False)
 bam= pysam.AlignmentFile(args.bam[0], "rb") 
 
-def get_reference = function(row,bam) :
+def get_reference(row,bam):
+    #print row
     chr=row["chr"]
     pos=int(row["pos"])
     py_pos=pos-1 # pythonify the position
     ref=row["ref"]
-    
+    fw=0
+    bw=0
+    cov_fw=0
+    cov_bw=0
+    for pileupcolumn in bam.pileup(chr,py_pos,py_pos+1,truncate=True,stepper="all",max_depth=1E6): # look at the range py_pos to py_pos+1 and all the bases that align to those positions. Think of a column on each position like the samtools output of mpileup
+        if pileupcolumn.pos==py_pos: #If the position is the one we want
+            for pileupread in pileupcolumn.pileups: #Now for each read in that column
+                if  not pileupread.is_del and not pileupread.is_refskip:
+                    if pileupread.alignment.is_reverse:
+                        called_base=pileupread.alignment.query_sequence[pileupread.query_position] # what is the called base at the position
+                        called_phred=pileupread.alignment.query_qualities[pileupread.query_position] # what is the phred of that base
+                        if called_phred>=30: # change this if you change the phred cut off in deepSNV. deepSNV only looks a phred>30. and we only want those calles that match the variant.
+                            cov_bw=cov_bw+1
+                            if called_base==ref:
+                                bw=bw+1
+                    if not pileupread.alignment.is_reverse:
+                        called_base=pileupread.alignment.query_sequence[pileupread.query_position] # what is the called base at the position
+                        called_phred=pileupread.alignment.query_qualities[pileupread.query_position] # what is the phred of that base
+                        if called_phred>=30: # change this if you change the phred cut off in deepSNV. deepSNV only looks a phred>30. and we only want those calles that match the variant.
+                            cov_fw=cov_fw+1
+                            if called_base==ref:
+                                fw=fw+1
+    print(fw)
+
+for index, row in variants.iterrows():
+    get_reference(row,bam)                                                    

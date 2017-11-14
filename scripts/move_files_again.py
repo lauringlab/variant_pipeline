3c1a1dbe3600987e20af60bed400403f6964276a
jtmccr1 <mccrone@umich.edu>
2017-11-13 20:18:43 -0500

    updating deepSNV and move files
    This change allow us to add a run name when moving files for the SRA
    
    They also now do not remove indels from the deepSNV results.
    We'll see if this causes changes down stream
    
    [ticket: X]

--- scripts/move_files_again.py
+++ scripts/move_files_again.py
@@ -2,13 +2,14 @@ import argparse
 import shutil
 import os
 import errno
-
+import re
 parser = argparse.ArgumentParser(description='So you\'ve run your analysis and deleted the copied fastq files to save space and now you need to run it again. This script takes in the renaming log file in the form initialfile\ttext\tfinal file and recopies the files. \n This is also useful in prepping to upload files to the SRA. by activating -sra_dir and -sra_file you can specify the samples and final directory for files you need to upload. This option appends the original run directory to the sample name for clarity.')
 parser.add_argument("s",nargs='+',metavar='s',help='The renaming file.')
 parser.add_argument("-r",action="store_true",dest='run', default=False,help = "Boolean switch to actually run the program.")
 parser.add_argument("-sra",action="store_true",dest='sra', default=False,help = "Boolean switch to activate SRA mode.")
 parser.add_argument("-sra_dir",action="store",dest='sra_dir',help = "The directory that will hold the SRA ready files")
 parser.add_argument("-sra_files",action="store",dest='sra_files',help = "A text file containing the sample IDs, (one per line). Final file names that include the sample ID will be copied")
+parser.add_argument("-run_name",action="store",dest='run_name',help = "Run name to add to sra file")
 
 args=parser.parse_args()
 def make_sure_path_exists(path):
@@ -40,7 +41,8 @@ def get_samples(sample_txt):
             samples.append(line)
     return(samples)
 
-def sracopy(rename_log,run,sra_dir,sra_files):
+def sracopy(rename_log,run,sra_dir,sra_files,run_name):
+    base_name = re.compile(r'(.*)(\.fastq.*$)')
     samples = get_samples(sra_files)
     with open(rename_log,'r') as file:
         for line in file:
@@ -49,13 +51,14 @@ def sracopy(rename_log,run,sra_dir,sra_files):
             new=line[2].strip()
             # adjust new to go to sra directory    
             file_name = new.split("/")[-1]
-            dir_name = new.split("/")[-2]
-        
-            final_new = sra_dir+"/"+dir_name+"_"+file_name
+            #dir_name = new.split("/")[-2]
+            
+            final_new = "%s/%s" %(sra_dir, base_name.sub("\\1.%s\\2" %run_name,file_name))
+            #final_new = sra_dir+"/"re.sub('\.fastq.*$', , somestring)+file_name+"_"+dir_name
 
             for sample in samples:
                     if sample in new:
-                       # print new
+                        print "sample: %s, new %s" %(sample,new)
                         if run==True:
                             shutil.copy2(old,final_new)
                         else:
@@ -65,7 +68,7 @@ def sracopy(rename_log,run,sra_dir,sra_files):
 def main():
     if args.sra : 
         make_sure_path_exists(args.sra_dir) # Make sra directory if needed
-        sracopy(args.s[0],args.run,args.sra_dir,args.sra_files)	
+        sracopy(args.s[0],args.run,args.sra_dir,args.sra_files,args.run_name)	
 
     else:
         recopy(args.s[0],args.run)

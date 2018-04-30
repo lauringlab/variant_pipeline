b97b20d82556bf7b8ddb2d6f384f67412011c9cf
JT McCrone <mccrone@umich.edu>
2018-04-30 15:01:09 -0400

    adds python pipeline

---
+++ scripts/python_pipeline.groovy
@@ -0,0 +1,20 @@
+
+
+// All the core pipeline stages in the pipeline
+load 'variantPipeline.bpipe.stages.groovy'
+
+done = {
+    exec "date"
+    exec "echo variantPipeline SUCCESS"
+}
+
+
+//For Hiseq data rename prior to running using the sample sheet and the change_hiseq_names.py found in scripts
+//For Miseq data where the sample name is derived from what we give the core use change_miseq_names.py
+Bpipe.run  {
+
+// QC on reads
+    "%.bam" * [ consensus ] +
+    "%.bam" * [ position_stats]+
+    done
+    }

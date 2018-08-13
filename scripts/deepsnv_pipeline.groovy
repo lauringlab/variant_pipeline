98e55e37861752fc88e3f8c0353826ea9095aba7
JT <mccrone@umich.edu>
2018-08-13 13:01:38 +0100

    adding forgotten deepSNV file

---
+++ scripts/deepsnv_pipeline.groovy
@@ -0,0 +1,17 @@
+// All the core pipeline stages in the pipeline
+load 'variantPipeline.bpipe.stages.groovy'
+
+done = {
+    exec "date"
+    exec "echo variantPipeline SUCCESS"
+}
+
+
+//For Hiseq data rename prior to running using the sample sheet and the change_hiseq_names.py found in scripts
+//For Miseq data where the sample name is derived from what we give the core use change_miseq_names.py
+Bpipe.run  {
+   "%.bam" * [deepsnv + "%.fasta" * [parse] + "%.csv"*[reference_base_and_quality]] +
+    "%.sum.csv"* [sift]+
+     "%.csv" * [classification] + combine +
+     done
+}
